140695974ac00c72942b75935199c09308db45a4
Wojciech Malota-Wojcik <wojtek@exw.co>
2024-08-26 13:49:51 +0200

    Fix spelling

--- types.go
+++ types.go
@@ -1,19 +1,19 @@
 package proton
 
-// Marshaler is the interface implemented by marshalers.
-type Marshaler interface {
-	Marshal(msg Marshalable, buf []byte) (uint64, uint64, error)
+// Marshaller is the interface implemented by marshallers.
+type Marshaller interface {
+	Marshal(msg Marshallable, buf []byte) (uint64, uint64, error)
 	Unmarshal(id uint64, buf []byte) (any, uint64, error)
 }
 
-// Marshalable is the interface for marshalable messages.
-type Marshalable interface {
+// Marshallable is the interface for marshallable messages.
+type Marshallable interface {
 	Marshal(buf []byte) uint64
 }
 
 // Message is the interface implemented by messages.
 type Message interface {
-	Marshalable
+	Marshallable
 
 	Size() uint64
 	Unmarshal(b []byte) uint64
